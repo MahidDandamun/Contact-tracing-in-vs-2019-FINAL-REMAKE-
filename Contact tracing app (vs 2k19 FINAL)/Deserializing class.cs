5f1b5b127aec792bdbcd3965eed220f6bd2d72fe
MahidDandamun <Dandamunmahid@gmail.com>
2022-07-09 00:17:14 +0800

    Added a class for deserialization purpose

---
+++ Contact tracing app (vs 2k19 FINAL)/Deserializing class.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Contact_tracing_app__vs_2k19_FINAL_
+{
+    class Deserializing_class
+    {
+        public string FullName { get; set; }
+        public string Address { get; set; }
+        public string Contact { get; set; }
+        public string Age { get; set; }
+        public string BodyTemperature { get; set; }
+        public string ZipCode { get; set; }
+        public string Email { get; set; }
+
+        public string Question1 { get; set; }
+        public string Question2 { get; set; }
+        public string Question3 { get; set; }
+        public string Question4 { get; set; }
+        public string Question5 { get; set; }
+        public string Question6 { get; set; }
+        public string Question7 { get; set; }
+    }
+}
